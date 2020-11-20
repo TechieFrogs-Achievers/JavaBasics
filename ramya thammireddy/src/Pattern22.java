95e51d667c4e22f40092274d6b21cde1e377ee6e
ramyareddy-thammireddy <ramyareddythammireddy99@gmail.com>
2020-11-20 10:32:42 +0530

    ramyat commit

--- ramya thammireddy/src/Pattern22.java
+++ ramya thammireddy/src/Pattern22.java
@@ -1,19 +1,22 @@
-public class Pattern1 
+public class Pattern22 
 {
     public static void main(String[] args)
     {
-        int alphabet = 65;
-        
+
         System.out.println("** Printing the pattern... **");
+
         for (int i = 0; i <= 5; i++)
         {
-            for (int j = 0; j <= i; j++)
+            int alphabet = 65;
+            for (int j = i; j <= 5; j++)
             {
                 System.out.print((char) (alphabet + j) + " ");
+            }
+            for (int k = 5 - 1; k >= i; k--)
+            {
+                System.out.print((char) (alphabet + k) + " ");
             }
             System.out.println();
         }
     }
 }
-    
-
---
+++ ramya thammireddy/src/Pattern27.java
@@ -0,0 +1,19 @@
+public class Pattern27 
+    {
+        public static void main(String[] args)
+        {
+            System.out.println("** Printing the pattern... **");
+    
+            for (int i = 0; i < 5; i++)
+            {
+                int alphabet = 65;
+                for (int j = 0; j < 5; j++)
+                {
+                    System.out.print((char) (alphabet + i+j));
+                }
+                System.out.println();
+            }
+        }
+    } 
+    
+
