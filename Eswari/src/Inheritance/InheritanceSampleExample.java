b52b650b92a89dcc32e78f711a0e2039ca5069bd
Ramaeswari <eswari.diddi22@gmail.com>
2020-11-23 23:23:24 +0530

    Inheritance Practice Programs

---
+++ Eswari/src/Inheritance/InheritanceSampleExample.java
@@ -0,0 +1,30 @@
+package Inheritance;
+
+//this is parent class
+class ParentClass
+{
+    //method for printing parent class
+    public void parentMethod()
+    {
+        System.out.println("This is parent class");
+    }
+}
+//this is child class. chlid class acquire properties of parent class by using extends keyword
+class ChildClass extends ParentClass
+{
+    //this is child method
+    public void childMethod()
+    {
+        System.out.println("This is chlid class");
+    }
+}
+public class InheritanceSampleExample 
+{  
+    public static void main(String[] args) {
+        ParentClass p = new ParentClass();      //parent class object creation
+        p.parentMethod();               //parent method calling by using parent class object
+        ChildClass c = new ChildClass();        //child class object creation
+        c.childMethod();                    //child method calling by using child class object
+        c.parentMethod();                    //parent method calling by using child class object
+    }   
+}
