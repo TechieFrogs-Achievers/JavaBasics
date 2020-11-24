f7de8587492ac0e41e1b31a36d0de1d85a104122
shirin-andukuri <shirinandukuri555@gmail.com>
2020-11-25 00:52:06 +0530

    methods & inheritance exercises

---
+++ Shirin/src/Variables1.java
@@ -0,0 +1,18 @@
+public class Variables1 
+{
+    int Value =200;
+    static int num=210;
+    public static void main(String[] args) 
+    {
+        int var =128;
+        System.out.println(var);
+        System.out.println(num);
+        Variables1 v = new Variables1();//creating object
+        System.out.println(v.Value);
+
+
+
+        
+    }
+    
+}
