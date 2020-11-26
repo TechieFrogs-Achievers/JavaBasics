367a867455f128d5690e5b9bc0efeb0b8b45b406
ramyakondapalli024 <ramyakondapalli407@gmail.com>
2020-11-26 16:07:41 +0530

    Inheritence

--- Ramya Kondapalli/src/Assignment4/Inheritence7.java
+++ Ramya Kondapalli/src/Assignment4/Inheritence7.java
@@ -1,21 +1,21 @@
 package Assignment4;
 //creating counter class
-class Counter1{
+class Counter1 {
     int i;
     void increment(int j){
         i=j;
-        System.out.println(i);
+        System.out.println("i = " + i);
     }
 }
 class Counter2 extends Counter1{
     void increment(){   //over riding method
-    this.increment(3);   //calling the method in parent class
+    super.increment(3);   //calling the method in parent class
     }
 }
 public class Inheritence7 {
     public static void main(String[] args) {
-    Counter1 c1 = new Counter1();
-    c1.i = 3; //intialising the data member through object 
-    System.out.println("i = " + c1.i);
+    Counter2 c2 = new Counter2();
+    //calling the method in 2nd class through object 
+    c2.increment();
     }
 }
