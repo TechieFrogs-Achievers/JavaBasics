07cc4f06e8d539c35a396468f967a03f5953e86d
DevipriyaNagasuri <devipriyanagasuri12@gmail.com>
2020-12-03 22:18:15 +0530

    Programs on scanner and string classes

---
+++ DeviPriya/src/String1.java
@@ -0,0 +1,35 @@
+public class String1 {
+    public static void main(String[] args) {
+        String line = "Devi priya";
+        int vowels = 0, consonants = 0, digits = 0, spaces = 0;
+
+        line = line.toLowerCase();
+        for(int i = 0; i < line.length(); ++i)
+        {
+            char ch = line.charAt(i);
+            if(ch == 'a' || ch == 'e' || ch == 'i'
+                || ch == 'o' || ch == 'u') {
+                ++vowels;
+            }
+            else if((ch >= 'a'&& ch <= 'z')) {
+                ++consonants;
+            }
+            else if( ch >= '0' && ch <= '9')
+            {
+                ++digits;
+            }
+            else if (ch ==' ')
+            {
+                ++spaces;
+            }
+        }
+
+        System.out.println("Vowels: " + vowels);
+        System.out.println("Consonants: " + consonants);
+        System.out.println("Digits: " + digits);
+        System.out.println("White spaces: " + spaces);
+    }
+}
+
+    
+
