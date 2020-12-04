a6a4ef0280e80c6f064eadd8bb4c58a8a034d2b6
Anushajakkam <anusha.jakkam123@gmail.com>
2020-12-04 11:06:09 +0530

    validation on scanner and strings two problems

---
+++ Anusha/src/ScannerClass/ValidationPart1.java
@@ -0,0 +1,9 @@
+package ScannerClass;
+import java.util.*;
+public class ValidationPart1 
+{
+   public static void main(String[] args) 
+   {
+       
+   } 
+}
---
+++ Anusha/src/Stringmethods/StringClass1.java
@@ -0,0 +1,42 @@
+package Stringmethods;
+import java.util.*;
+public class StringClass1 
+{
+     public static void main(String[] args) 
+ {
+    int vowels = 0, consonants = 0, digits = 0, spaces = 0,specialChar = 0;
+    Scanner sc=new Scanner(System.in);//ScannerClass
+    System.out.println("Enter string:");
+    String str=sc.nextLine();
+    // str.length() function to count number of 
+    // character in given string.
+    for (int i = 0; i < str.length(); i++) 
+    { 
+        char ch = str.charAt(i); 
+          if ( (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z') ) 
+            { 
+              // To handle upper case letters 
+                ch = Character.toLowerCase(ch); 
+              //check the vowels 
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') 
+                vowels++; 
+            else
+                consonants++; 
+        } 
+        else if (ch >= '0' && ch <= '9') //check digits
+               digits++; 
+        else if (ch == '!' || ch == '@' || ch == '#' || ch == '$' || ch == '&' || ch=='*')//check specialChar
+              specialChar++;
+        else
+            spaces++; 
+    } 
+      
+    System.out.println("Vowels: " + vowels); 
+    System.out.println("Consonant: " + consonants); 
+    System.out.println("Digit: " + digits); 
+    System.out.println("Spaces: " + spaces); 
+    System.out.println("specialChar: " + specialChar); 
+
+ }       
+        
+}
