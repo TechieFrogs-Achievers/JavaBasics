7bd49564c7e06d61f1c3762acc4b407dbe01b764
JameemaBandaru <jameemabandaru09@gmail.com>
2020-12-05 23:51:07 +0530

    String Buffer Exercise

--- Jameema/src/Strings/AnagramString.java
+++ Jameema/src/Strings/AnagramString.java
@@ -6,14 +6,14 @@ import java.util.Scanner;
 
 //import javax.lang.model.util.ElementScanner14;
 
-public class StringExercise9
+public class AnagramString
  {
      public static void main(String[] args)
       {
           System.out.println("enter first string "); //enter string
           Scanner sc = new Scanner(System.in); //scanner class
           String s1=sc.nextLine();
-          System.out.println("enter second String");
+          System.out.println("enter second  String");
           String s2=sc.nextLine();
           if(s1.length()==s2.length()) //both stringlenghts are equal
           {
