2e2215646f2557546763d6dce47781225467b9a0
DevipriyaNagasuri <devipriyanagasuri12@gmail.com>
2020-12-10 00:26:19 +0530

    Remaining programs on Arrays

--- DeviPriya/src/Array4.java
+++ DeviPriya/src/Array4.java
@@ -5,9 +5,10 @@ public class Array4 {
         for(int i = 0; i < arr.length; i++) {  
         for(int j = i + 1; j < arr.length; j++) {  
         if(arr[i] == arr[j])  
-        System.out.println(arr[j]);  
+        System.out.println(arr[j]); 
+           
         }  
         }  
-        }  
+    } 
     
 }
