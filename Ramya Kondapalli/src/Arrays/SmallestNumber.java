f92889a68daa2dabfa7aa488f3057a32cdb45881
ramyakondapalli024 <ramyakondapalli407@gmail.com>
2020-12-10 00:51:39 +0530

    Array Programs

--- Ramya Kondapalli/src/Arrays/SmallestNumber.java
+++ Ramya Kondapalli/src/Arrays/SmallestNumber.java
@@ -4,9 +4,6 @@ public class SmallestNumber {
     public static int getSmallestNumber(int[] arr,int pos){
         int temp;   //temporaray variable
 
-        // int thirdlargestNumber = 3,secondLargestNumber = 2;
-
-
         //loops for sorting array in ascending order
         for (int i=0;i<arr.length;i++){
 
@@ -20,13 +17,16 @@ public class SmallestNumber {
                 }
             }
         }
-
-        return (arr[pos]);
+         
+        return (arr[pos]); //retrun the position of value
     } 
     public static void main(String[] args) {
 
         int[] arr = new int[] {12,35,84,62,28,93,15};
+
+        //method to get 2nd largest number
         System.out.println("2nd smallest number : " + getSmallestNumber(arr,1));
+        //method to get 1st largest number
         System.out.println("Smallest number : " + getSmallestNumber(arr,0));
         
     }
