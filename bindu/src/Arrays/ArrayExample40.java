55b8a5f1d05312700ef1e39424eeeda8a2c478fc
HimaBinduKetha <himabinduketha33@gmail.com>
2020-12-12 09:19:27 +0530

    Sample Exceptions Programs

---
+++ bindu/src/Arrays/ArrayExample40.java
@@ -0,0 +1,38 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class ArrayExample40 
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("Enter the length");
+        Scanner sc = new Scanner(System.in);      // scanner class
+        int length = sc.nextInt();      // array length
+        System.out.println(" Enter the element");
+        int array[] = new int[length];     // Initiallizing array for stroring the data element
+        for(int i =0; i < array.length; i++)
+        {
+            array[i] = sc.nextInt();    // Reading elements
+        }
+        int sum = 0;     // Initiallizing variable 
+        double standarddev = 0.0;     
+
+        for( int i =0; i < array.length; i++)    // loop for  checking the elements
+        {
+            sum = sum+array[i];           // Adding the elements into sum
+        }
+        int mean = sum/array.length;      // calculating mean 
+        System.out.println();
+        System.out.println("mean value :" +mean);
+
+        for(int i =0; i < array.length; i++)
+        {
+            standarddev = standarddev +Math.pow(array[i]-mean, 2);     // standard deviation condition
+        }
+        System.out.println(Math.sqrt(standarddev/array.length));
+        sc.close();  // scanner close
+        
+    }
+    
+}
---
+++ bindu/src/ExceptionHandling/Exception7.java
@@ -0,0 +1,38 @@
+package ExceptionHandling;
+
+public class Exception7 
+{
+    public static void main(String[] args) 
+    {
+        try 
+        {
+            test();    //method creation
+        
+        }
+        catch(ApplicationException ae)   //catch block that holds the exception from try block
+        {
+            System.out.println(ae.getMessage());
+        }
+    }
+    public static void test() throws ApplicationException
+    {
+        try 
+        {
+            int i=10;
+            int j=0;
+            System.out.println(i/j);   
+            
+        } 
+        catch(Exception e)
+        {
+            throw new ApplicationException(e);
+        }
+    } 
+}
+class ApplicationException extends Exception
+{
+    public ApplicationException(Exception e)
+    {
+        super(e);
+    }
+}
